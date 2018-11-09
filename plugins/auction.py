b1c6389a016166ca57fb9dfe5e2d0c92490d32aa
msciotti <mason.sciotti@gmail.com>
2018-11-09 11:38:23 -0800

    first

---
+++ plugins/auction.py
@@ -0,0 +1,37 @@
+from disco.bot import Plugin
+import requests
+import json
+
+class AuctionPlugin(Plugin):
+
+  @Plugin.command('search', '[item_name:str...]')
+  def on_auction_Search(self, event, item_name):
+    r = requests.post('https://na.nasomi.com/auctionhouse/data/ah-data/searchItemByName.php',
+                      data={
+                        'itemname': item_name
+                      })
+
+
+    search_response = r.json()
+    if not search_response['list']:
+      event.msg.reply('Could not find listings for {}'.format(item_name))
+      return
+
+    item_id = search_response['list'][0]['itemid']
+
+    r = requests.post('https://na.nasomi.com/auctionhouse/data/ah-data/searchItem.php',
+                      data={
+                        'itemid': item_id,
+                        'stack': 1
+                      })
+
+    price_response = r.json()
+    items = price_response['sale_list']
+    total = 0
+    count = 0
+    for price in items:
+      total += int(price['price'])
+      count += 1
+    total = total / count
+
+    event.msg.reply('Current {} price: {} gil'.format(item_name, total))
