542b8b1519edec1505a0ca14dd3114db7e9c7e5f
msciotti <mason.sciotti@gmail.com>
2018-11-11 23:48:06 -0800

    Move to edenxi

--- plugins/auction.py
+++ plugins/auction.py
@@ -1,42 +1,43 @@
 from disco.bot import Plugin
 import requests
 import json
+import math
+import urllib
+
+SEARCH_URL = 'http://classicffxi.com/ajax/'
 
 class AuctionPlugin(Plugin):
 
   @Plugin.command('search', '[item_name:str...]')
   def on_auction_Search(self, event, item_name):
-    r = requests.post('https://na.nasomi.com/auctionhouse/data/ah-data/searchItemByName.php',
-                      data={
-                        'itemname': item_name
-                      })
-
+    formatted_item_name = item_name.lower().replace(' ', '_')
+    qs = {
+      'name': item_name
+    }
+    r = requests.get('{}items'.format(SEARCH_URL), params=qs)
 
     search_response = r.json()
-    if not search_response['list']:
+    if not search_response:
       event.msg.reply('Could not find listings for {}'.format(item_name))
       return
 
-    item_id = search_response['list'][0]['itemid']
-    data_params = {
-      'itemid': item_id
+    real_item = [obj for obj in search_response if obj['name'] == formatted_item_name][0]
+    item_id = real_item['id']
+    payload = {
+      'id': item_id
     }
 
-    stack = search_response['list'][0]['stackSize']
-
-    if int(stack) != 1:
-      data_params['stack'] = 1
-
-    r = requests.post('https://na.nasomi.com/auctionhouse/data/ah-data/searchItem.php',
-                      data=data_params)
+    r = requests.get('{}item'.format(SEARCH_URL), params=payload)
 
     price_response = r.json()
-    items = price_response['sale_list']
-    total = 0
-    count = 0
+    items = price_response['ahhistory']
+    if not items:
+      event.msg.reply('No price history found for {}'.format(item_name))
+      return
+    total = []
     for price in items:
-      total += int(price['price'])
-      count += 1
-    total = total / count
+      total.append(int(price['sale']))
+    total.sort()
+    median = total[int(math.floor(len(total)/2))]
 
-    event.msg.reply('Current {} price: {} gil'.format(item_name, total))
+    event.msg.reply('Median {} price: {} gil'.format(item_name, median))
